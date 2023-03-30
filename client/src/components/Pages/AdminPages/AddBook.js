73d06986d5bc64c0a5538015915d6699ad59934a
youssefayad23 <119113349+youssefayad23@users.noreply.github.com>
2023-03-30 17:48:33 +0200

    add page addBook front-end

--- client/src/components/Pages/AdminPages/AddBook.js
+++ client/src/components/Pages/AdminPages/AddBook.js
@@ -10,7 +10,27 @@ const AddBook = () => {
     <>
       <Header />
       <div>AddBook</div>
-      
+      <div className="container mt-3">
+        <h1>Upload Your Img Here</h1>
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>UserName</Form.Label>
+            <Form.Control type="text" name="fname" /* onChange={setdata}  */ />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Select Your Image</Form.Label>
+            <Form.Control
+              type="file"
+              name="photo" /*  onChange={setimgfile} */
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" /*  onClick={addUserData} */>
+            Submit
+          </Button>
+        </Form>
+      </div>
     </>
   );
 };
